994d8c0f41838e7dbc99643b1e86fcbd05ced9cc
Adetayo <twinebravo@gmail.com>
2024-11-29 23:29:34 +0100

    Updated React JS file

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -5,52 +5,60 @@ import { Link } from "react-router-dom";
 
 
 const Login = () =>{
-const userDetails = {
-    email: "",
-    password: "",
-  };
-
-  const [data, setData] = useState(userDetails);
-
-  function handleChange(event){
-    const {name, value} = event.target
-    //setData((prevData))=>({...prevData, [name]:value}));
-    setData((prevData)=>{
-      return {...prevData, [name]: value}
-    })
-  }
-
-  return (
-    <div>
-      <form action="">
-        <div>
-          <input
-            type="text"
-            name="email"
-            placeholder="Enter Your Email"
-            className={style.input}
-            onChange = {handleChange}
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your Password"
-            className={style.input}
-            onChange = {handleChange}
-           required
-          />
-        </div>
-        <CustomButton style={style.btn} type="submit" textContent="Login"/>
-      </form>
-        <div>
-          <span>Have no account?</span>
-          <span><Link to={'/signup'}>Sign Up</Link></span>
-        </div>
-    </div>
-  );
-  }
+  const userDetails = {
+      email: "",
+      password: "",
+    };
   
-  export default Login
+    const [data, setData] = useState(userDetails);
+  
+    function handleChange(event){
+      const {name, value} = event.target
+      //setData((prevData))=>({...prevData, [name]:value}));
+      setData((prevData)=>{
+        return {...prevData, [name]: value}
+      })
+    }
+  
+    return (
+      <body class ={style.loginBackground}>
+        <div class={style.center_box}>
+              <div class={style.logintitle}
+              >Film App Login
+              </div>
+                  <div class={style.login_box}>
+                    <form action="">
+                      <div>
+                        <input
+                          type="text"
+                          name="email"
+                          placeholder="Enter Your Email"
+                          className={style.input}
+                          onChange = {handleChange}
+                          required
+                        />
+                      </div>
+                      <div>
+                        <input
+                          type="password"
+                          name="password"
+                          placeholder="Enter your Password"
+                          className={style.input}
+                          onChange = {handleChange}
+                        required
+                        />
+                      </div>
+                      <CustomButton style={style.btn} type="submit" textContent="Login"/>
+                    </form>
+                    </div>
+                  <div class={style.no_account}>
+                    <span>Have no account?</span>
+                    <span><Link class={style.nonedeco} to={'/signup'}>  Sign Up </Link></span>
+                  </div>
+              </div>
+          </body>
+    );
+    }
+    
+    export default Login
+    
--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -4,7 +4,6 @@ import CustomButton from "../reusables/CustomButton";
 import { Link , useNavigate} from "react-router-dom";
 
 const SignUp = () => {
-
   const navigate = useNavigate();
 
   const userDetails = {
@@ -17,13 +16,13 @@ const SignUp = () => {
 
   function handleChange(event){
     const {name, value} = event.target
-    // setData((prevData)=>({...prevData, [name]:value}));
+    //setData((prevData)=>({...prevData, [name]:value}));
     setData((prevData)=>{
       return {...prevData, [name]: value}
     });
   };
   console.log(data)
-  
+    
 
   const handleSubmit = () =>{
     console.log("Submitted......");
@@ -33,46 +32,53 @@ const SignUp = () => {
   }
 
 
-  return (
-    <div>
-      <form  onSubmit={handleSubmit} action="">
-        <div>
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter Username"
-            className={style.input}
-            onChange = {handleChange}
-            //value={data.username}
-            required
-          />
+return (
+  <body class ={style.signupBackground}>
+      <div class={style.center_box}>
+          <div class={style.signuptitle}
+          >Film App Sign Up
+          </div>
+              <div class={style.signup_box}>
+                  <form  onSubmit={handleSubmit} action="">
+                      <div>
+                      <input
+                          type="text"
+                          name="username"
+                          placeholder=" Enter Username"
+                          className={style.input}
+                          onchange = {handleChange}
+                          required
+                      />
+                      </div>
+                      <div>
+                      <input
+                          type="text"
+                          name="email"
+                          placeholder=" Enter Email"
+                          className="input"
+                          required
+                      />
+                      </div>
+                      <div>
+                      <input
+                          type="password"
+                          name="password"
+                          placeholder=" Enter Password"
+                          className="input"
+                          required
+                      />
+                      </div>
+                      <CustomButton style={style.btn} type="submit" textContent="submit"/>
+                  </form>
+              </div>   
+              <div class={style.own_account}>
+                  <span>Already have an account?</span>
+                  <span><Link class={style.nonedeco} to={'/login'}> Login</Link></span>
+              </div> 
         </div>
-        <div>
-          <input
-            type="text"
-            name="email"
-            placeholder="Enter Email"
-            className="input"
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter Password"
-            className="input"
-            required
-          />
-        </div>
-        <CustomButton style={style.btn} type="submit" textContent="submit"/>
-      </form>
-      <div>
-        <span>Already have an account?</span>
-        <span><Link to={'/login'}>Login</Link></span>
-      </div>
-    </div>
-  );
+      {/* </div> */}
+  </body>
+);
 };
 
 export default SignUp;
